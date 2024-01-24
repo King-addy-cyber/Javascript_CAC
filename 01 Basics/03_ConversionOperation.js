c0a6ffd20eecdb7390a4e118e1b90405ab545d43
Adarsh Sharma <69235560+King-addy-cyber@users.noreply.github.com>
2024-01-24 16:11:53 +0000

    Conversion

---
+++ 01 Basics/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+// let score = "35abcd"
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log (valueInNumber);
+
+let boolean =0
+
+let booleanValue =Boolean(boolean)
+// console.log(booleanValue);
+
+let num = 55
+
+let strNum = String(num)
+console.log(strNum);
+console.log(typeof (strNum));
