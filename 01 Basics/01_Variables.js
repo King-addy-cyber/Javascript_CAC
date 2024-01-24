b9ec6e06292820288a3ed863a9e6cfff3a3dd021
Adarsh Sharma <69235560+King-addy-cyber@users.noreply.github.com>
2024-01-24 15:58:31 +0000

    ok

---
+++ 01 Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 145585
+let accountEmail = "name_sirname@gmail.com"
+var accountPassword = "1234568"
+accountCity ="Jaipur"
+let accountState
+
+// accountId = 5
+
+accountEmail = "sirname@gmail.com"
+accountPassword = "12368"
+accountCity ="Delhi"
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword = "1234568"
+  ,  accountCity ,accountState ])
