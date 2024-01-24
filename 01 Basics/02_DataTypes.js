63d5191d5a32abf5f2a9b613d48bb9bc3b623c64
Adarsh Sharma <69235560+King-addy-cyber@users.noreply.github.com>
2024-01-24 15:58:12 +0000

    DataTypes

---
+++ 01 Basics/02_DataTypes.js
@@ -0,0 +1,15 @@
+"use strict" //treate all js code as newer version
+
+// alert(3 + 5) //we are using nodejs , not browser
+
+let name = "Adarsh" //string
+let age = 25 //number
+let isLoggedIn = false //boolean
+//bigint
+//null =>standalone value | it as an type of object
+//undefined =>value not assigned
+//symbol => unique
+
+//objects
+
+console.log(typeof "Adarsh");
