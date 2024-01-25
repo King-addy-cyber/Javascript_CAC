35f77367f9282f8a645da9c6761fdf5ec531c4c1
Adarsh Sharma <69235560+King-addy-cyber@users.noreply.github.com>
2024-01-25 22:22:17 +0530

    Maths in Js

---
+++ 01 Basics/05_string.js
@@ -0,0 +1,15 @@
+const name = "Adarsh Sharma ";
+const age = 25;
+
+// console.log(name + age);
+//String interpolation
+// console.log(`Hello my name is ${name}and I am ${age}`);
+
+const Name = new String("Adarsh-Sharma");
+// const Name = new String("Adarsh-Sharma");
+// Name;
+//to get object properties in console.
+console.log(name[5]);
+console.log(Name.__proto__);
+console.log(Name.charAt(4));
+console.log(Name.indexOf("a"));
---
+++ 01 Basics/06_Numbers_and _Maths.js
@@ -0,0 +1,26 @@
+// const score = 254;
+// console.log(score);
+// const balance = new Number(12540.411);
+// console.log(typeof balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(2));
+
+// const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString());
+
+// ------------------- Maths in Js -------------------
+
+// console.log(Math)
+// console.log(Math.abs(-58))
+// console.log(Math.round(65.84))
+// console.log(Math.ceil(65.24))
+// console.log(Math.floor(65.24))
+console.log(Math.random());
+
+//To generate values between two numbers
+const min = 10;
+const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
